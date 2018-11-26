3c6adfe0b4eb4dffb21a54a4412e7f1d1947765b
github username <github email address>
2018-11-26 15:11:29 -0500

    done

--- run.rb
+++ run.rb
@@ -2,9 +2,13 @@ require_relative "lib/Animal.rb"
 require_relative "lib/Zoo.rb"
 require 'pry'
 
-
-#Test your code here
-
+brZoo = Zoo.new("MegaZoo", "DUMBO")
+nz = Zoo.new("notAZoo", "DUMBO")
+cz = Zoo.new("Circus", "DUMBO")
+wz = Zoo.new("dumb", "not dumbo")
+fluffy = Animal.new("Cat", 2, "Fluff", brZoo)
+farty = Animal.new("Dog", 5, "Pelusa", brZoo)
+tony = Animal.new("Dog", 6, "Tony", brZoo)
 
 binding.pry
-puts "done"
+puts cat
